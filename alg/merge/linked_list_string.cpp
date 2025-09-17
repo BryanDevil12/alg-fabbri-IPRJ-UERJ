4e18f76071158f34b6e9fd057300c37fac0268f2
BryanDevil12 <bryan-devil12@hotmail.com>
2025-09-17 12:27:00 -0300

    teste

--- alg/merge/linked_list_string.cpp
+++ alg/merge/linked_list_string.cpp
@@ -1,21 +1,87 @@
 /* R. Fabbri, 2024 */
-#include "sorted_merge_3way.h"
-#include <climits>
+#include "linked_list_string.h"
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 
-bool
-sorted_merge_3way(
-    const int *list_a, int na,
-    const int *list_b, int nb,
-    const int *list_c, int nc,
-    int *list_abc)
-{
-
-  /* 
-
-   Entrar coom aqui 
-   <++>
-   
+#define MAX_NUMERO_DE_CARACTERES 64
+struct No {
+  //char *name;
+  char name[MAX_NUMERO_DE_CARACTERES];
+  /*
+   opcional: convencao de '\0' no final
   */
+  No *next;
+};
 
-  return false; // stub
+typedef No *Lista;
+
+// ---------------------------------------------------------------
+// mesma lista, mas com ponteiro pra char
+struct No2 {
+  char *name;
+  //char name[MAX_NUMERO_DE_CARACTERES];
+  /*
+   opcional: convencao de '\0' no final
+  */
+  No *next;
+};
+
+typedef No2 *Lista2;
+
+void 
+playground_lista()
+{
+  std::cout << "teste" << std::endl;
+  Lista l;
+
+  // cria um novo no
+  
+  l = (No *) malloc(sizeof(No));
+
+  // no caso da implementacao estatica
+  l->name[0] = '\0';
+  
+  // ou no caso do char * :
+  //l->name = "";
+  //l->name = 0;
+
+  std::cout << std::string(l->name) << std::endl;
+
+  std::cout << "teste" << std::endl;
+  // ou
+  printf("%s", l->name); // assume que nome contem '\0'
+  
+  strncpy(l->name, "joao", MAX_NUMERO_DE_CARACTERES);
+  
+  printf("%s", l->name); // assume que nome contem '\0'
+}
+
+void 
+playground_lista2()
+{
+  // Exercicio1: prencher com lista 2
+  std::cout << "teste" << std::endl;
+  Lista l;
+
+  // cria um novo no
+  
+  l = (No *) malloc(sizeof(No));
+
+  // no caso da implementacao estatica
+  l->name[0] = '\0';
+  
+  // ou no caso do char * :
+  //l->name = "";
+  //l->name = 0;
+
+  std::cout << std::string(l->name) << std::endl;
+
+  std::cout << "teste" << std::endl;
+  // ou
+  printf("%s", l->name); // assume que nome contem '\0'
+  
+  strncpy(l->name, "joao", MAX_NUMERO_DE_CARACTERES);
+  
+  printf("%s", l->name); // assume que nome contem '\0'
 }
